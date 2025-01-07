8f1385c386fed5833b02a98637b1cf4cf458e1f4
abdel <abdelhakim.moustabchir@gmail.com>
2025-01-07 09:12:45 +0100

    init commit back

---
+++ src/main/java/com/gestionrepas/GestionRepasApplication.java
@@ -0,0 +1,13 @@
+package com.gestionrepas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionRepasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionRepasApplication.class, args);
+    }
+
+}
