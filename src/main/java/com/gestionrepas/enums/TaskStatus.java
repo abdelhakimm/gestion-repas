ea1b935e4f6d00ed89562fdbe4d4f2dc6a363eb1
abdel <abdelhakim.moustabchir@gmail.com>
2025-01-15 14:19:29 +0100

    enum task status finish

--- src/main/java/com/gestionrepas/enums/TaskStatus.java
+++ src/main/java/com/gestionrepas/enums/TaskStatus.java
@@ -1,4 +1,17 @@
 package com.gestionrepas.enums;
 
 public enum TaskStatus {
+    TODO("À faire"),
+    IN_PROGRESS("En cours"),
+    DONE("Terminé");
+
+    private final String displayName;
+
+    TaskStatus(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
 }
